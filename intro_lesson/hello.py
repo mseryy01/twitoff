f7e89408ebc39f59b0baa0b981507c89cab84aec
Mikhail Seryy <mseryy01@users.noreply.github.com>
2019-12-16 20:35:44 -0400

    add users and tweets

--- intro_lesson/hello.py
+++ intro_lesson/hello.py
@@ -1,12 +1,20 @@
-""" Minimal Flask App"""
-from flask import flask
+"""Minimal flask app"""
 
-#Make App
+from flask import Flask, render_template
+
+#Make the application
 app = Flask(__name__)
 
-# Make the route
+#Make the route
 @app.route("/")
 
-#Define function
+#Now define a function
 def hello():
-    return "Hello World!"
+    return render_template('home.html')
+
+#Make a second route
+@app.route("/about")
+
+#Now make the function that goes with about
+def preds():
+    return render_template('about.html')
